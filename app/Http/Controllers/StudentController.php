a8a6025f3786d54ba3aee885b93c78f278a9f4cd
nazi sharifi <banonazisharifi@gmail.com>
2026-01-09 16:34:50 +0430

    added work by where

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -13,4 +13,17 @@ class StudentController extends Controller
         Gate::authorize('show' , $student);
         return $student;
     }
+    public function where(){
+    //    $st = student::where('score','>' , 50)->get();
+    //    student::where('userId',5)->orWhere('userId',2)->get();
+    // student::whereBetween('id', 12,20)->get();
+    // student::whereIn('gender',[1,2,3,4,5,6,])->get();
+    // student::where('name','like','%A')->get();
+    // student::where('gender',"!=" , "male")->get();
+    // student::where('gender','=','female')->orderBy('name','asc')->get(); desc
+    // student::where('gender','male')->where(function ($static) {
+    //    $static->where('name','like','%N%')->orWhere('gender','female');
+    // })->get();
+    //    return $st ;
+    }
 }
